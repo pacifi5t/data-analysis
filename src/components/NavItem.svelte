4db1bae16c20ab29aae63e97235334274a326b0b
Andrey Dergach <thequasar7@gmail.com>
2021-10-01 02:49:14 +0300

    Fixed text wrapping in navbar

--- src/components/NavItem.svelte
+++ src/components/NavItem.svelte
@@ -7,13 +7,13 @@
 {#if currentRoute.name === href}
   <a
     {href}
-    class="bg-gray-900 text-white px-3 py-2 rounded-md text-lg font-medium"
+    class="bg-gray-900 text-white px-3 py-2 whitespace-nowrap rounded-md text-lg font-medium"
     aria-current="page">{label}</a
   >
 {:else}
   <a
     {href}
-    class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-lg font-medium"
+    class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 whitespace-nowrap rounded-md text-lg font-medium"
     aria-current="page">{label}</a
   >
 {/if}
