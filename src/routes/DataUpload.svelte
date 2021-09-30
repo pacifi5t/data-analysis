e1baa4be6c61f1f0550699b5db3a53c1ec26a1b1
Andrey Dergach <thequasar7@gmail.com>
2021-09-30 20:09:32 +0300

    Added file dropzone

--- src/routes/DataUpload.svelte
+++ src/routes/DataUpload.svelte
@@ -1,4 +1,9 @@
 <script lang="ts">
-  export let currentRoute;
+  import { FileDropzone } from 'attractions';
   
+  let files = [];
 </script>
+
+<div class="text-xl font-bold">
+  <FileDropzone accept="text/*" {files} max={1} />
+</div>
