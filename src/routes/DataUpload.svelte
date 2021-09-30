e055fbc26e829f20f2f2966711e192a9976b1112
Andrey Dergach <thequasar7@gmail.com>
2021-10-01 02:46:30 +0300

    Data is now kept with stores

--- src/routes/DataUpload.svelte
+++ src/routes/DataUpload.svelte
@@ -1,9 +1,47 @@
 <script lang="ts">
   import { FileDropzone } from 'attractions';
-  
-  let files = [];
+  import { immutableDataStore, fileStore } from '../stores';
+
+  const reader = new FileReader();
+  let uplodedFiles = [];
+  let immutableData = [];
+
+  fileStore.subscribe((value) => {
+    uplodedFiles = value;
+  });
+  immutableDataStore.subscribe((value) => {
+    immutableData = value;
+  });
 </script>
 
 <div class="text-xl font-bold">
-  <FileDropzone accept="text/*" {files} max={1} />
+  <FileDropzone
+    accept="text/*"
+    max={1}
+    files={uplodedFiles}
+    on:change={(event) => {
+      fileStore.set(event.detail.files);
+      console.log(uplodedFiles);
+
+      if (uplodedFiles.length != 0) {
+        reader.readAsText(uplodedFiles[0]);
+
+        reader.onload = () => {
+          let str = '' + reader.result;
+          let data = [];
+          str.split('\n').forEach((value) => {
+            data.push(Number.parseFloat(value));
+          });
+          immutableDataStore.set(data);
+        };
+      } else {
+        immutableDataStore.set([]);
+      }
+    }}
+  />
 </div>
+
+{#if immutableData.length != 0}
+  <!-- TODO: Reimplement display of the data -->
+  <p>{immutableData.toString()}</p>
+{/if}
