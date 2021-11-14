5c71af15c6cbdf61ec299ec21b2c07a9b1b01c78
Andrey Dergach <thequasar7@gmail.com>
2021-11-14 18:38:56 +0200

    Added functions for metrics

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -15,6 +15,39 @@ export function stdDev(series: VarSeries, mean: number) {
   );
 }
 
+export function median(series: VarSeries) {
+  const array = [...series.initialArray].sort((a, b) => a - b);
+  const len = array.length;
+  return len % 2 != 0 ? array[(len + 1) / 2] : array[len / 2 + len / 2 + 1] / 2;
+}
+
+export function skewnessCoef(
+  series: VarSeries,
+  stdDeviation: number,
+  mean: number
+) {
+  return (
+    series.initialArray.reduce((total, x) => total + Math.pow(x - mean, 3), 0) /
+    (series.length * Math.pow(stdDeviation, 3))
+  );
+}
+
+export function kurtosisCoef(
+  series: VarSeries,
+  stdDeviation: number,
+  mean: number
+) {
+  return (
+    series.initialArray.reduce((total, x) => total + Math.pow(x - mean, 4), 0) /
+      (series.length * Math.pow(stdDeviation, 4)) -
+    3
+  );
+}
+
+export function antikurtosisCoef(kurtosisCoefficient: number) {
+  return 1 / Math.sqrt(kurtosisCoefficient + 3);
+}
+
 export function kde(bandwidth: number, series: VarSeries, limits: number[]) {
   if (series.length == 0) {
     return [];
