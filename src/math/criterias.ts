1ceb78057fc95d7972c5f90cf1c668ac61766b54
Andrey Dergach <thequasar7@gmail.com>
2021-12-04 12:32:08 +0200

    Implemented mean equality criterias

--- src/math/criterias.ts
+++ src/math/criterias.ts
@@ -1,7 +1,36 @@
 import { jStat } from "jstat";
 import { alpha, fisherDistribQuan, mean, stdDev } from ".";
 
-export function dispersionEquality(arrX, arrY) {
+export function depMeanEq(arrX: number[], arrY: number[]) {
+  const arrZ = [];
+  for (let i = 0; i < arrX.length; i++) {
+    arrZ.push(arrX[i] - arrY[i]);
+  }
+  const zMean = mean(arrZ);
+  const zStd = stdDev(arrZ, zMean);
+  const t = (zMean * Math.sqrt(arrZ.length)) / zStd;
+  const p = 2 * (1 - jStat.ttest(t, arrZ.length - 1, 2));
+
+  return p >= alpha;
+}
+
+export function indepMeanEq(arrX: number[], arrY: number[]) {
+  const xMean = mean(arrX);
+  const yMean = mean(arrY);
+  const v = arrX.length + arrY.length - 2;
+  const stdDeviation =
+    ((arrX.length - 1) * stdDev(arrX, xMean) +
+      (arrY.length - 1) * stdDev(arrY, yMean)) /
+    v;
+  const t =
+    (xMean - yMean) /
+    Math.sqrt(stdDeviation / arrX.length + stdDeviation / arrY.length);
+  const p = 2 * (1 - jStat.ttest(t, v, 2));
+
+  return p >= alpha;
+}
+
+export function dispersionEq(arrX: number[], arrY: number[]) {
   const f =
     Math.pow(stdDev(arrX, mean(arrX)), 2) /
     Math.pow(stdDev(arrY, mean(arrY)), 2);
