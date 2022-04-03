c70d7854c7b1a9f639f039d165d9fe5bbab5184d
Andrey Dergach <thequasar7@gmail.com>
2022-04-04 02:20:51 +0300

    Calculate correlation ratio estimate & f-statistic

--- src/math/correlation.ts
+++ src/math/correlation.ts
@@ -1,6 +1,12 @@
+import { max, min, sum } from "d3";
 import { mean, shiftedDeviation } from ".";
 import { alpha } from "./confidence-intervals";
 import { normDistribQuan } from "./quantiles";
+
+export interface CorrelationArray {
+  x: number[];
+  y: number[][];
+}
 
 export function pearsonCorrelationEstimate(arrX: number[], arrY: number[]) {
   const xMean = mean(arrX);
@@ -26,4 +32,80 @@ export function pearsonCorrelationInterval(
 
 export function tStatistic(len: number, estimate: number) {
   return (estimate * Math.sqrt(len - 2)) / Math.sqrt(1 - Math.pow(estimate, 2));
+}
+
+export function correlationRatioTransformation(
+  arrX: number[],
+  arrY: number[]
+): CorrelationArray {
+  const minX = min(arrX);
+  const k = 1 + 1.44 * Math.log(arrX.length);
+  const h = (max(arrX) - minX) / k;
+
+  const arrG = [];
+  for (let i = 1; i <= k + 1; i++) {
+    arrG.push(minX + (i - 1) * h);
+  }
+
+  const transArrX: number[] = [];
+  for (let i = 0; i < k; i++) {
+    transArrX.push((arrG[i] + arrG[i + 1]) / 2);
+  }
+
+  const transArrY: number[][] = [];
+  for (let i = 0; i < k; i++) {
+    transArrY.push([]);
+  }
+
+  for (let i = 0; i < arrX.length; i++) {
+    for (let j = 0; j < k; j++) {
+      if (arrX[i] < arrG[j + 1]) {
+        transArrY[j].push(arrY[i]);
+      }
+    }
+  }
+
+  return { x: transArrX, y: transArrY };
+}
+
+export function correlationRatioEstimate(
+  len: number,
+  correlationArray: CorrelationArray
+) {
+  const classCount = correlationArray.x.length;
+
+  const meanYGroup = [];
+  for (let i = 0; i < classCount; i++) {
+    const group = correlationArray.y[i];
+    meanYGroup.push(sum(group) / group.length);
+  }
+
+  const meanY = correlationArray.y.reduce(
+    (total, elem, i) => total + elem.length * meanYGroup[i],
+    0
+  );
+
+  const nom = correlationArray.y.reduce(
+    (total, elem, i) =>
+      total + elem.length * Math.pow(meanYGroup[i] - meanY, 2),
+    0
+  );
+
+  const denom = correlationArray.y.reduce(
+    (total1, arr) =>
+      total1 +
+      arr.reduce((total2, y) => {
+        return total2 + Math.pow(y - meanY, 2);
+      }, 0),
+    0
+  );
+
+  return Math.sqrt(nom / denom);
+}
+
+export function fStatistic(len: number, classCount: number, estimate: number) {
+  const estimateSqr = Math.pow(estimate, 2);
+  return (
+    estimateSqr / (classCount - 1) / ((1 - estimateSqr) / (len - classCount))
+  );
 }
