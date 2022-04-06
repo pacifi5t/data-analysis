d141a6b03e0e8667ad368a5b53a74673bfb4dda5
Andrey Dergach <thequasar7@gmail.com>
2022-04-06 14:39:05 +0300

    Remove unused code, move updateCharacteristicsTable out from helpers.ts

--- src/routes/Characteristics.svelte
+++ src/routes/Characteristics.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
   import { Table } from "attractions";
-  import type * as mymath from "../math";
-  import { updateCharacteristicsTable } from "../utils/helpers";
+  import { pretty } from "../utils/helpers";
   import { mutableSamplesStore } from "../utils/stores";
+  import * as mymath from "../math";
 
   const headers = [
     { text: "", value: "title" },
@@ -12,19 +12,95 @@
   ];
 
   let mutableSamples: mymath.VarSeries[] = [];
-  let items = [];
+  let tableItems = [];
 
   mutableSamplesStore.subscribe((value: mymath.VarSeries[]) => {
     mutableSamples = value;
     if (value.length != 0) {
       for (let index = 0; index < value.length; index++) {
-        items[index] = updateCharacteristicsTable(mutableSamples[index]);
+        tableItems[index] = updateCharacteristicsTable(mutableSamples[index]);
       }
     }
   });
+
+  function updateCharacteristicsTable(sample: mymath.VarSeries) {
+    const len = sample.initialArray.length;
+    const array = sample.initialArray;
+    const items = [];
+
+    const meanValue = mymath.mean(array);
+    const stdDeviation = mymath.stdDev(array, meanValue);
+    const meanStdDev = mymath.meanDeviation(sample.length, stdDeviation);
+    const meanInterval = mymath.meanConfInterval(
+      sample.length,
+      meanStdDev,
+      meanValue
+    );
+    items.push({
+      title: "Mean",
+      val: pretty(meanValue),
+      stddev: pretty(meanStdDev),
+      conf: `${pretty(meanInterval[0])} ; ${pretty(meanInterval[1])}`
+    });
+
+    const medianValue = mymath.median(array);
+    const medianInterval = mymath.medianConfInterval(array);
+    items.push({
+      title: "Median",
+      val: pretty(medianValue),
+      stddev: "-",
+      conf: `${pretty(medianInterval[0])} ; ${pretty(medianInterval[1])}`
+    });
+
+    const stdDevDeviation = mymath.stdDevDeviation(len, stdDeviation);
+    const stdDevConfInterval = mymath.stdDevConfInterval(
+      len,
+      stdDeviation,
+      stdDevDeviation
+    );
+    items.push({
+      title: "Standard deviation",
+      val: pretty(stdDeviation),
+      stddev: pretty(stdDevDeviation),
+      conf: `${pretty(stdDevConfInterval[0])} ; ${pretty(
+        stdDevConfInterval[1]
+      )}`
+    });
+
+    const shiftedDev = mymath.shiftedDeviation(array, meanValue);
+    const skewnessCoef = mymath.skewnessCoef(array, shiftedDev, meanValue);
+    const skewnessDeviation = mymath.skewnessDeviation(len);
+    const skewnessInterval = mymath.coefConfInterval(
+      len,
+      skewnessCoef,
+      skewnessDeviation
+    );
+    items.push({
+      title: "Skewness coefficient",
+      val: pretty(skewnessCoef),
+      stddev: pretty(skewnessDeviation),
+      conf: `${pretty(skewnessInterval[0])} ; ${pretty(skewnessInterval[1])}`
+    });
+
+    const kurtosisCoef = mymath.kurtosisCoef(array, shiftedDev, meanValue);
+    const kurtosisDeviation = mymath.kurtosisDeviation(len);
+    const kurtosisInterval = mymath.coefConfInterval(
+      len,
+      kurtosisCoef,
+      kurtosisDeviation
+    );
+    items.push({
+      title: "Kurtosis coefficient",
+      val: pretty(kurtosisCoef),
+      stddev: pretty(kurtosisDeviation),
+      conf: `${pretty(kurtosisInterval[0])} ; ${pretty(kurtosisInterval[1])}`
+    });
+
+    return items;
+  }
 </script>
 
-{#each items as item, i}
+{#each tableItems as item, i}
   <div class="flex flex-row">
     <span class="text-center mx-2">
       ATTRIBUTE {i + 1}
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,4 +1,3 @@
-import * as mymath from "../math";
 import { ClassifiedSeries, VarSeries } from "../math/series";
 
 export function pretty(num: number): number {
@@ -42,80 +41,6 @@ export function updateClassifiedSeries(
   });
 
   return new ClassifiedSeries(classCount, limits, classifiedArray);
-}
-
-export function updateCharacteristicsTable(sample: VarSeries) {
-  const items = [];
-  const len = sample.initialArray.length;
-  const array = sample.initialArray;
-
-  const meanValue = mymath.mean(array);
-  const stdDeviation = mymath.stdDev(array, meanValue);
-  const meanStdDev = mymath.meanDeviation(sample.length, stdDeviation);
-  const meanInterval = mymath.meanConfInterval(
-    sample.length,
-    meanStdDev,
-    meanValue
-  );
-  items.push({
-    title: "Mean",
-    val: pretty(meanValue),
-    stddev: pretty(meanStdDev),
-    conf: `${pretty(meanInterval[0])} ; ${pretty(meanInterval[1])}`
-  });
-
-  const medianValue = mymath.median(array);
-  const medianInterval = mymath.medianConfInterval(array);
-  items.push({
-    title: "Median",
-    val: pretty(medianValue),
-    stddev: "-",
-    conf: `${pretty(medianInterval[0])} ; ${pretty(medianInterval[1])}`
-  });
-
-  const stdDevDeviation = mymath.stdDevDeviation(len, stdDeviation);
-  const stdDevConfInterval = mymath.stdDevConfInterval(
-    len,
-    stdDeviation,
-    stdDevDeviation
-  );
-  items.push({
-    title: "Standard deviation",
-    val: pretty(stdDeviation),
-    stddev: pretty(stdDevDeviation),
-    conf: `${pretty(stdDevConfInterval[0])} ; ${pretty(stdDevConfInterval[1])}`
-  });
-
-  const shiftedDev = mymath.shiftedDeviation(array, meanValue);
-  const skewnessCoef = mymath.skewnessCoef(array, shiftedDev, meanValue);
-  const skewnessDeviation = mymath.skewnessDeviation(len);
-  const skewnessInterval = mymath.coefConfInterval(
-    len,
-    skewnessCoef,
-    skewnessDeviation
-  );
-  items.push({
-    title: "Skewness coefficient",
-    val: pretty(skewnessCoef),
-    stddev: pretty(skewnessDeviation),
-    conf: `${pretty(skewnessInterval[0])} ; ${pretty(skewnessInterval[1])}`
-  });
-
-  const kurtosisCoef = mymath.kurtosisCoef(array, shiftedDev, meanValue);
-  const kurtosisDeviation = mymath.kurtosisDeviation(len);
-  const kurtosisInterval = mymath.coefConfInterval(
-    len,
-    kurtosisCoef,
-    kurtosisDeviation
-  );
-  items.push({
-    title: "Kurtosis coefficient",
-    val: pretty(kurtosisCoef),
-    stddev: pretty(kurtosisDeviation),
-    conf: `${pretty(kurtosisInterval[0])} ; ${pretty(kurtosisInterval[1])}`
-  });
-
-  return items;
 }
 
 export function getDifferenceSample(sampleX: VarSeries, sampleY: VarSeries) {
